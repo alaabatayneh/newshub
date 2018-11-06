f6ae01ca490fd8e82990ee1eb409053675661684
Alaa Batayneh <a.batayneh@gmail.com>
2018-11-06 14:05:37 +0300

    first commit

---
+++ precache-manifest.e3170598e96540e8d4ee7a0be4a9c279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd580d65b0031d712c42",
+    "url": "./static/css/main.cf869086.chunk.css"
+  },
+  {
+    "revision": "cd580d65b0031d712c42",
+    "url": "./static/js/main.cd580d65.chunk.js"
+  },
+  {
+    "revision": "6f948089d2e2fb89d717",
+    "url": "./static/js/1.6f948089.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "./static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "7476fe0a6335c3b98a295bc8d26ffc7a",
+    "url": "./index.html"
+  }
+];
